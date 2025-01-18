c65c2bdc643cf47fe7643136fbaecfc9b2cc4cdd
Ben Pearson <benpearson61@googlemail.com>
2025-01-18 17:51:25 +0900

    add books table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_18_081912) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_18_084710) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,7 +19,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_18_081912) do
     t.bigint "listing_id", null: false
     t.date "start_date"
     t.date "end_date"
-    t.string "status"
+    t.integer "status"
     t.integer "total"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -48,6 +48,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_18_081912) do
     t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "books_id", null: false
+    t.index ["books_id"], name: "index_listings_on_books_id"
     t.index ["user_id"], name: "index_listings_on_user_id"
   end
 
@@ -76,6 +78,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_18_081912) do
 
   add_foreign_key "bookings", "listings"
   add_foreign_key "bookings", "users"
+  add_foreign_key "listings", "books", column: "books_id"
   add_foreign_key "listings", "users"
   add_foreign_key "reservation_reviews", "bookings", column: "bookings_id"
   add_foreign_key "reservation_reviews", "users"
