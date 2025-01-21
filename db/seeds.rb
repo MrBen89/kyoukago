de0c738e86788ce79cf4d60fe9e137d58829aee5
Ben Pearson <benpearson61@googlemail.com>
2025-01-21 21:51:28 +0900

    fixed my db screw-ups. seeds work now

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,17 +11,18 @@
 require 'httparty'
 
 puts "Clearing database..."
+Booking.destroy_all
+Listing.destroy_all
 User.destroy_all
 Book.destroy_all
-Booking.destroy_all
-Review.destroy_all
+ReservationReview.destroy_all
 
 puts "Creating users..."
 users = [
-  { email: "buyer1@example.com", password: "password", role: "buyer" },
-  { email: "buyer2@example.com", password: "password", role: "buyer" },
-  { email: "seller1@example.com", password: "password", role: "seller" },
-  { email: "seller2@example.com", password: "password", role: "seller" }
+  { email: "buyer1@example.com", password: "password" },
+  { email: "buyer2@example.com", password: "password" },
+  { email: "seller1@example.com", password: "password" },
+  { email: "seller2@example.com", password: "password" }
 ]
 users.each { |user| User.create!(user) }
 
@@ -31,38 +32,50 @@ books_data = response.parsed_response["docs"]
 
 
 puts "Creating books..."
-sellers = User.where(role: "seller")
+sellers = User.all
 books_data.each do |book_data|
   Book.create!(
     title: book_data["title"],
     author: book_data["author_name"]&.join(", ") || "Unknown Author",
-    description: book_data["first_sentence"] || "No description available.",
-    price: rand(10..50),
-    rental_available: [true, false].sample,
-    image_url: "https://covers.openlibrary.org/b/id/#{book_data['cover_i']}-L.jpg",
-    user: sellers.sample
+  )
+end
+
+
+puts "Creating listings..."
+buyers = User.all
+books = Book.all
+books.sample(5).each do |book|
+  Listing.create!(
+    book: book,
+    user: buyers.sample,
+    title: "yes",
+    price: 350,
+    condition: "Used",
+    comment: "delicious"
   )
 end
 
 puts "Creating bookings..."
-buyers = User.where(role: "buyer")
-books = Book.all
-books.sample(5).each do |book|
+buyers = User.all
+listings = Listing.all
+listings.sample(5).each do |listing|
   Booking.create!(
-    book: book,
+    listing: listing,
     user: buyers.sample,
-    status: "pending"
+    status: "pending",
+    total: 350
   )
 end
 
 puts "Creating reviews..."
-books.each do |book|
+bookings = Booking.all
+listings.each do |listing|
   rand(1..3).times do
-    Review.create!(
-      book: book,
+    ReservationReview.create!(
+      booking: bookings.sample,
       user: buyers.sample,
-      rating: rand(3..5),
-      content: "This book is cool! nice."
+      score: rand(3..5),
+      comment: "This book is cool! nice."
     )
   end
 end
