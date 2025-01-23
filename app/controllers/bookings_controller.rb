cf749e6a9aade21fea1d94baa85b936edbc2f266
Ben Pearson <benpearson61@googlemail.com>
2025-01-23 19:41:04 +0900

    Listings and bookings controller added

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,13 +6,6 @@ class BookingsController < ApplicationController
     redirect_to listings_path, status: :see_other
   end
 
-  def create
-    @booking = Booking.new(booking_params)
-    @booking.user = current_user
-    @booking.save
-    redirect_to listings_path, status: :see_other
-  end
-
   private
 
   def bookings_param
