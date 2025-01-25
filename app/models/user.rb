5401a0e09bd770c967b1178bb6a3e3d406117d78
Julian Cass <j.cass@outlook.com>
2025-01-25 11:27:25 +0900

    Save changes to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
   has_many :listings
+  has_one_attached :profile_image
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
 end
