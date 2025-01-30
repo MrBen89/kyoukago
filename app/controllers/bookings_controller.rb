81f283a4dd7417feeca1eed06f6019365fbe0367
Liam Gingell <elgee95@gmail.com>
2025-01-30 21:20:09 +0900

    Merge pull request #43 from MrBen89/booking-price-calculation
    Booking price calculation

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,15 +1,17 @@
 class BookingsController < ApplicationController
   def create
-    @booking = Booking.new(bookings_param)
-    @booking.user = current_user
-    @booking.status = 0
-    @booking.save
-    redirect_to listings_path, status: :see_other
+    @listing = Listing.find(params[:listing_id])
+    @booking = @listing.bookings.build(booking_params)
+    if @booking.save
+      redirect_to @listing, notice: 'Booking was successfully created.'
+    else
+      render 'listings/show'
+    end
   end
 
   private
 
-  def bookings_param
-    params.require(:booking).permit(:listing_id, :start_date, :end_date, :status, :total)
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :total_price)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+
 ActiveRecord::Schema[7.1].define(version: 2025_01_30_104922) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +53,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_30_104922) do
     t.integer "total"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "total_price"
     t.index ["listing_id"], name: "index_bookings_on_listing_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
