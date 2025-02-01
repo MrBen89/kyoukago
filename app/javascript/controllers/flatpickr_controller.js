62505343812bc36336eb3ff7cc67f360d1377ed3
Liam Gingell <elgee95@gmail.com>
2025-02-01 14:06:03 +0900

    Added calendar

---
+++ app/javascript/controllers/flatpickr_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "@hotwired/stimulus";
+import flatpickr from "flatpickr";
+
+export default class extends Controller {
+  static targets = ["startDate"];
+
+  connect() {
+  console.log(this.startDateTarget);
+
+  console.log("flatpickr controller connected");
+
+    flatpickr(this.startDateTarget, {
+      // enableTime: false,
+      // dateFormat: "Y-m-d",
+      // altInput: true,
+      enableTime: true,
+      dateFormat: "Y-m-d H:i",
+    });
+  }
+}
